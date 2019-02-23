254d697d7aeb079a4aab5c6146019cc336e90d3e
AaronReina <aaron.ivan.reina@gmail.com>
2019-02-23 18:59:05 +0100

    test

--- client/src/lib/auth.js
+++ client/src/lib/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-   baseURL: process.env.NODE_ENV === "production" ? "": 'https://localhost:3000',
+   baseURL: process.env.NODE_ENV == "production" ? "": 'https://localhost:3000',
     
     withCredentials: true,
 });
